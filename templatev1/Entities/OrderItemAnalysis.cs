f9fcea7220f39d7e5b769060be86589f43ffbd11
mythic3011 <me@mythic3011.xyz>
2024-06-24 01:15:10 +0800

    add dataset for generate report

---
+++ templatev1/Entities/OrderItemAnalysis.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using controller;
+using controller.Utilities;
+
+namespace templatev1.Entities
+{
+    partial class OrderItemAnalysis
+    {
+        partial class OrderTableDataTable
+        {
+        }
+    }
+}
+
+namespace templatev1.Entities.OrderItemAnalysisTableAdapters
+{
+    public partial class itemTableTableAdapter
+    {
+
+    }
+
+    public partial class OrderTableTableAdapter
+    {
+
+    }
+}
