d04f3c49f65e40a507278d183c018704fb83182c
mythic3011 <me@mythic3011.xyz>
2024-06-25 18:03:11 +0800

    add TestForm to Test Form

---
+++ TestController/TestForm.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Forms;
+using templatev1;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace TestController
+{
+    public class TestForm
+    {
+        // test login form
+        [Fact]
+        public void TestLoginForm()
+        {
+            Form loginForm = new Login();
+            loginForm.Show();
+            Assert.True(loginForm.Visible);
+        }
+    }
+}
